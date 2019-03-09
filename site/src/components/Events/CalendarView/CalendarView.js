0b979231fb9cb68bdb5576f5d1731f76422174d7
Jessica Borja <jessicalizethborja@hotmail.com>
2019-03-09 16:51:22 -0700

    deleted unnecesary files

--- site/src/components/Events/CalendarView/CalendarView.js
+++ site/src/components/Events/CalendarView/CalendarView.js
@@ -4,6 +4,7 @@ import BigCalendar from 'react-big-calendar'
 import moment from 'moment'
 import myEventsList from './events'
 import './CalendarView.css'
+
 // Setup the localizer by providing the moment (or globalize) Object
 // to the correct localizer.
 const localizer = BigCalendar.momentLocalizer(moment) // or globalizeLocalizer
