19181a6658d682372c0734f003038eab051d1f82
Jessica Borja <jessicalizethborja@hotmail.com>
2019-03-23 22:16:12 -0700

    design changes

--- site/src/components/Home/Services/Services.js
+++ site/src/components/Home/Services/Services.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Col, Container, Badge } from "react-bootstrap";
 import "./Services.css";
 
 import servicios320w from "../../../assets/images/landing-page/servicios-320w.jpg";
@@ -10,34 +10,45 @@ const Services = () => {
     <Container fluid className="home__services">
       <Row>
         <Col
-          sm={3}
+          sm={8}
           xs={12}
           className="home__services-list-container"
         >
-          <h2>Servicios</h2>
-          <ul>
-            <li className="selected">Consejería familiar y matrimonial</li>
-            <li>Guía espiritual</li>
-            <li>Ayuda a enfermos</li>
-            <li>
-              Ayuda a familias Vulnerables, inmigrantes o de bajos recursos.
-            </li>
-            <li>Asistencia a Personas de la tercera edad</li>
-          </ul>
+          <div className="home__services-list-card">
+            <div>
+              <h2>Servicios</h2>
+              <p>
+                En nuestra iglesia nos preocupamos por nuestra comunidad y ofrecemos
+                ayuda a familias Vulnerables, inmigrantes o de bajos recursos.
+              </p>
+            </div>
+
+            <div className="home__services-list-container-bullets">
+              <div>
+                <ul>
+                  <li>Ayuda a familias Vulnerables, inmigrantes o de bajos recursos</li>
+                  <li>Guía espiritual</li>
+                  <li>Ayuda a enfermos</li>
+                </ul>
+              </div>
+
+              <div>
+                <ul>
+                  <li>Asistencia a Personas de la tercera edad</li>
+                  <li>Consejería familiar y matrimonial</li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </Col>
-        <Col sm={6} xs={12} className="home__services-details">
-          <h2>Ayuda a familias Vulnerables</h2>
-          <p>
-            En nuestra iglesia nos preocupamos por nuestra comunidad y ofrecemos
-            ayuda a familias Vulnerables, inmigrantes o de bajos recursos.
-          </p>
-        </Col>
-        <Col sm={3} xs={12} className="home__services-img-container">
-          <img
-            src={servicios320w}
-            srcSet={`${servicios320w} 320w, ${servicios640w} 640w`}
-            alt="Service"
-          />
+        <Col sm={4} xs={12} className="p-0" >
+          <div className="home__services-img-container">
+            <img
+              src={servicios320w}
+              srcSet={`${servicios320w} 320w, ${servicios640w} 640w`}
+              alt="Service"
+            />
+          </div>
         </Col>
       </Row>
     </Container>
